0bd91cbf94425551e07e56a3c5bae3589346f920
jarvis007friday <sathwikvalluripally916@gmail.com>
2024-07-01 15:16:51 +0530

    files updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://satwik-school.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
